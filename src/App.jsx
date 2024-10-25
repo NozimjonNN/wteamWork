de7e2994e1d55cf61f7bd3695b6946d177e6d18f
Nozimjon <masteryt123@mail.ru>
2024-10-25 22:50:03 +0500

    fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
 import PP from '../components/Nozimjon/PP';
+import Unfound from '../components/Nozimjon/unfound';
 import '../global.css'
 
 export default function App() {
   return(
     <>
       <Routes>
-        <Route path='/' element={<PP />} />
-        {/* <Route path='/' element={<Home />} />
+        <Route path='/pp' element={<PP />} />
         <Route path='/' element={<Home />} />
         <Route path='/' element={<Home />} />
         <Route path='/' element={<Home />} />
-        <Route path='*' element={<Home />} /> */}
+        <Route path='/' element={<Home />} />
+        <Route path='*' element={<Unfound />} />
       </Routes>
     </>
   )
