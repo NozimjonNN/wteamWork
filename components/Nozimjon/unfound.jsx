33a2738815c87bd3542aefb68ecd252a1e574773
Nozimjon <masteryt123@mail.ru>
2024-10-25 22:48:52 +0500

    Nozimjon have finished his work

---
+++ components/Nozimjon/unfound.jsx
@@ -0,0 +1,11 @@
+import  './pp.css'
+
+export default function Unfound() {
+    return(
+        <>
+            <div className="main">
+                <h1 className='wh'>ERROR 404 | NOF FOUND</h1>
+            </div>
+        </>
+    )
+}
