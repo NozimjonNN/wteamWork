f99fd84e8c408f7722f6f48262bc7a44e9e49597
Asad <Asad@gmail.com>
2024-10-26 18:27:46 +0500

    asadbek ishimi boldim

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import PP from '../components/Nozimjon/PP';
 import Unfound from '../components/Nozimjon/unfound';
 import '../global.css'
-
+import Home from './componets/Asadber/Home';
 export default function App() {
   return(
     <>
